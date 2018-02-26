4e71181ed03a74b72af80e6ebf77e1a5d9a3c160
staxx6 <staxx@hotmail.de>
2018-02-26 22:53:12 +0100

    update

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 
@@ -16,13 +17,13 @@ def post_detail(request, pk):
     context = {'post':post}
     return render(request, 'blog/post_detail.html', context)
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,3 +31,36 @@ def post_new(request):
 
     context = {'form': form}
     return render(request, 'blog/post_edit.html', context)
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form =  PostForm(instance=post)
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context)
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    context = {'posts': posts}
+    return render(request, 'blog/post_draft_list.html', context)
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
