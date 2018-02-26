97a0bf07625e8a4353184f9162ff740b0822de73
staxx6 <staxx@hotmail.de>
2018-02-26 02:25:35 +0100

    missused for a tut

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .models import Post
+
+from .forms import PostForm
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=
+        timezone.now()).order_by('-published_date')
+    context = {'posts':posts}
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post':post}
+    return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+
+    context = {'form': form}
+    return render(request, 'blog/post_edit.html', context)
